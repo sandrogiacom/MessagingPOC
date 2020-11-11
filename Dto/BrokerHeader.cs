e9838f19f28f63a305229ac4d776789b3debbf41
Sandro Giacomozzi <sandro.giacomozzi@fluig.com>
2020-11-11 12:08:32 -0300

    JsonPropertyName

--- Dto/BrokerHeader.cs
+++ Dto/BrokerHeader.cs
@@ -1,8 +1,12 @@
-﻿namespace MessagingPOC.Dto
+﻿using System.Text.Json.Serialization;
+
+namespace MessagingPOC.Dto
 {
     class BrokerHeader
     {
+        [JsonPropertyName("tenant")]
         public string Tenant { get; set; }
+        [JsonPropertyName("user")]
         public string User { get; set; }
     }
 }
--- Dto/BrokerMessage.cs
+++ Dto/BrokerMessage.cs
@@ -1,11 +1,14 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using Tnf.Bus.Client;
 
 namespace MessagingPOC.Dto
 {
     class BrokerMessage : Message
     {
+        [JsonPropertyName("header")]
         public BrokerHeader Header { get; set; }
+        [JsonPropertyName("body")]
         public object Body { get; set; }
 
         public BrokerMessage(BrokerHeader header, object body)
